afef65de2d45016a73e2afb39d9feb62bfe5beb2
matthew.jones@slimmingworld.com <matthew.jones@slimmingworld.com>
2015-09-17 15:01:30 +0100

    Excercies 7 - Media API

--- Umbraco.Course/Controllers/StatusUpdateController.cs
+++ Umbraco.Course/Controllers/StatusUpdateController.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using Umbraco.Course.Level2;
 using Umbraco.Course.Models;
+using System.Collections.Generic;
 
 namespace Umbraco.Course.Controllers
 {
@@ -19,6 +22,36 @@ namespace Umbraco.Course.Controllers
             var statusUpdate = Services.ContentService.CreateContent(name, CurrentPage.Id, "statusUpdate");
 
             statusUpdate.SetValue("bodyText", model.BodyText);
+
+            if (model.Files.HasFiles() && model.Files.ContainsImages())
+            {
+                string statusFolderName = "Status updates";
+                var statusFolder = Services.MediaService.GetChildren(-1).FirstOrDefault(m => m.Name == statusFolderName);
+
+                if (statusFolder == null)
+                {
+                    //create the folder for all of the status updates
+                    statusFolder = Services.MediaService.CreateMedia(statusFolderName, -1, "Folder");
+                    Services.MediaService.Save(statusFolder);
+                }
+
+                var folder = Services.MediaService.CreateMedia(Guid.NewGuid().ToString(), statusFolder.Id, "Folder");
+                Services.MediaService.Save(folder);
+
+                var ids = new List<string>();
+
+                foreach (var file in model.Files.Where(f => f.IsImage()))
+                {
+                    var image = Services.MediaService.CreateMedia(file.FileName, folder.Id, "Image");
+                    image.SetValue("umbracoFile", file);
+                    Services.MediaService.Save(image);
+                    ids.Add(image.Id.ToString());
+                }
+
+                string joinedIds = string.Join(",", ids);
+
+                statusUpdate.SetValue("relatedMedia", joinedIds);
+            }
 
             var publishStatus = Services.ContentService.PublishWithStatus(statusUpdate);
 
