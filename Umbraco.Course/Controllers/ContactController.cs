baf0894c80f9886de3d368f7f21e48b6315ac1e0
matthew.jones@slimmingworld.com <matthew.jones@slimmingworld.com>
2015-09-17 12:23:24 +0100

    Excercise 5 interim checkin

---
+++ Umbraco.Course/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Course.Models;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Course.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult Submit(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SaveMessage(model);
+                TempData["Success"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            return CurrentUmbracoPage();
+        }
+
+        private void SaveMessage(ContactModel model)
+        {
+            //TODO: Save message
+        }
+    }
+}
