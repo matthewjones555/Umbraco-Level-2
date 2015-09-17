7e5ea5315f9bda6c6e228e3c1119fe7113ea837a
matthew.jones@slimmingworld.com <matthew.jones@slimmingworld.com>
2015-09-17 12:00:52 +0100

    Level 2 initial commit

---
+++ Umbraco.Course/Controllers/StatusUpdateController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Course.Level2;
+using Umbraco.Course.Models;
+
+namespace Umbraco.Course.Controllers
+{
+    public class StatusUpdateController : SurfaceController
+    {
+        public ActionResult Create(StatusUpdateModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+            
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
