9f747b7294f00769faa19d2a278583b75d6fc46a
matthew.jones@slimmingworld.com <matthew.jones@slimmingworld.com>
2015-09-17 14:01:31 +0100

    Excercise 7

--- Umbraco.Course/Controllers/StatusUpdateController.cs
+++ Umbraco.Course/Controllers/StatusUpdateController.cs
@@ -10,8 +10,18 @@ namespace Umbraco.Course.Controllers
         public ActionResult Create(StatusUpdateModel model)
         {
             if (!ModelState.IsValid)
+            {
                 return CurrentUmbracoPage();
-            
+            }
+
+            string name = Umbraco.Truncate(Umbraco.StripHtml(model.BodyText), 50, false).ToString();
+
+            var statusUpdate = Services.ContentService.CreateContent(name, CurrentPage.Id, "statusUpdate");
+
+            statusUpdate.SetValue("bodyText", model.BodyText);
+
+            var publishStatus = Services.ContentService.PublishWithStatus(statusUpdate);
+
             return RedirectToCurrentUmbracoPage();
         }
     }
