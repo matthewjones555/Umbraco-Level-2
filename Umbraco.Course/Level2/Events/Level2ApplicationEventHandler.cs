215c4c56c5a1e52395adc84bbdc3b755133c6e4e
matthew.jones@slimmingworld.com <matthew.jones@slimmingworld.com>
2015-09-18 12:18:21 +0100

    Exercise 13 - Events

--- Umbraco.Course/Level2/Events/Level2ApplicationEventHandler.cs
+++ Umbraco.Course/Level2/Events/Level2ApplicationEventHandler.cs
@@ -11,8 +11,53 @@ namespace Umbraco.Course.Level2.Events
     {
         protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication,ApplicationContext applicationContext)
         {
-            
+            ContentService.Saving += ContentService_Saving;
+            ContentService.Publishing += ContentService_Publishing;
         }
 
+        private void ContentService_Saving(IContentService sender, SaveEventArgs<IContent> e)
+        {
+            foreach (var content in e.SavedEntities)
+            {
+                if (content.ContentType.Alias == "statusUpdate")
+                {
+                    var bodyText = content.GetValue<string>("bodyText");
+
+                    if (bodyText.Contains("ass"))
+                    {
+                        bodyText = bodyText.Replace("ass", "butt");
+                        content.SetValue("bodyText", bodyText);
+                    }
+
+                    if (BAD_WORDS.Any(b => bodyText.ToLower().Contains(b)))
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        readonly string[] BAD_WORDS = new string[]
+        {
+            "fudge", "gosh", "darn", "heck", "ass", 
+        };
+
+        private void ContentService_Publishing(IPublishingStrategy sender, PublishEventArgs<IContent> e)
+        {
+            foreach (var content in e.PublishedEntities)
+            {
+                if (content.ContentType.Alias == "statusUpdate")
+                {
+                    var bodyText = content.GetValue<string>("bodyText").ToLower();
+
+                    if (BAD_WORDS.Any(b => bodyText.Contains(b)))
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
